ba407274ab32ec27fed61c55c740a409bd14c6cc
Jesse Dijkman <jesse.dijkman@hva.nl>
2018-06-14 17:25:51 +0200

    Finished the layout for the root page, now ready to convert it to an ejs file

--- static/js/transitions.js
+++ static/js/transitions.js
@@ -1,8 +1,33 @@
 /* jshint esversion: 6 */
 
-const filterBtn = document.getElementById('filter-btn');
-const filter = document.getElementById('filter-container');
+const radioButtons = document.querySelectorAll('#sorting input[type="radio"]');
+const likeButtons = document.querySelectorAll('#recipe-list article footer ul li form button');
+const sortButton = document.getElementById('sort-btn');
+const sortingForm = document.getElementById('sorting');
 
-filterBtn.addEventListener('click', () => {
-  filter.classList.toggle('hidden');
+sortButton.addEventListener('click', (ev) => {
+  sortingForm.classList.toggle('hidden');
 });
+
+(function() {
+  for (let i = 0; i < radioButtons.length; i++) {
+    radioButtons[i].addEventListener('click', () => {
+
+      for (let i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked == true) {
+          radioButtons[i].parentElement.classList.add('check');
+        } else {
+          radioButtons[i].parentElement.classList.remove('check');
+        }
+      }
+    });
+  }
+})();
+
+(function() {
+  for (let i = 0; i < likeButtons.length; i++) {
+    likeButtons[i].addEventListener('click', () => {
+      likeButtons[i].classList.toggle('liked');
+    });
+  }
+})();
