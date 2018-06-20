21ab8533a294d27a5f2ae6751624c7539a5e7f37
Jesse Dijkman <jesse.dijkman@hva.nl>
2018-06-20 15:01:18 +0200

    Can now make an account. The users' input is checked when creating an account. Can't loggin yet

---
+++ checkinputs.js
@@ -0,0 +1,92 @@
+/* jshint esversion: 6 */
+/* jshint node: true */
+const passwordValidator = require('password-validator');
+const emailValidator = require('email-validator');
+
+const requirementsPassword = new passwordValidator();
+
+requirementsPassword
+  .is().min(5)
+  .is().max(50)
+  // .has().uppercase()
+  .has().lowercase()
+  // .has().digits()
+  .has().not().spaces();
+
+let rx = /[!@#$%^&*.()_+\-=\[\]{};:\\|<>\/?\`\~ (0-9)]/;
+
+module.exports = function(inputData) {
+  let keys = Object.keys(inputData);
+  let result = {
+    error: {},
+    data: {},
+    msg: {}
+  };
+
+  keys.forEach((key) => {
+    if (inputData[key].length == 0) {
+      result.error[key] = 'isEmpty';
+      result.data[key] = null;
+      result.msg[key] = 'Missing';
+    } else {
+
+      if (key == 'user_firstName' || key == 'user_lastName') {
+        if (inputData[key].search(rx) >= 0) {
+          result.error[key] = 'isInvalid';
+          result.data[key] = null;
+          result.msg[key] = `Can't have special characters, numbers or spaces`;
+        } else {
+          // result.error[key] = '';
+          result.data[key] = inputData[key];
+          // result.msg[key] = '';
+        }
+      } else if (key == 'user_email') {
+        if (emailValidator.validate(inputData[key]) == false) {
+          result.error[key] = 'isInvalid';
+          result.data[key] = null;
+          result.msg[key] = 'Email is incorrect';
+        } else {
+          result.data[key] = inputData[key];
+        }
+      } else if (key == 'user_password') {
+        result.data[key] = null;
+        result.data.user_passwordConf = null;
+
+        let fails = requirementsPassword.validate(inputData[key], {
+          list:true
+        });
+
+        fails.forEach((fail) => {
+
+          switch (fail) {
+            case 'min':
+              result.error.user_password = 'isInvalid';
+              result.msg.user_password = 'Must be more than 8 characters long';
+              break;
+            case 'spaces':
+              result.error.user_password = 'isInvalid';
+              result.msg.user_password = `Can't have spaces`;
+              break;
+            case 'digits':
+              result.error.user_password = 'isInvalid';
+              result.msg.user_password = 'Password must have numbers';
+              break;
+            case 'uppercase':
+              result.error.user_password = 'isInvalid';
+              result.msg.user_password = 'Password must have uppercase characters';
+              break;
+          }
+        });
+
+
+      } else if (inputData.user_password !== inputData.user_passwordConf) {
+        result.error.user_password = 'isInvalid';
+        result.error.user_passwordConf = 'isInvalid';
+        result.msg.user_password = `Passwords don't match`;
+      }
+    }
+
+  });
+
+  return result;
+};
--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const mysql = require('mysql');
+// const salt = bcrypt.genSaltSync(10);
+
+const checker = require('./checkinputs.js');
+
+require('dotenv').config();
+
+
+// MySQL connection
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true
+});
+
+connection.connect();
 
 const app = express();
 
@@ -25,8 +43,31 @@ app.get('/', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-  console.log('yes');
-  res.render('register.ejs');
+  let result = {
+    error: {
+      user_firstName: null,
+      user_lastName: null,
+      user_email: null,
+      user_password: null,
+      user_passwordConf: null
+    },
+    data: {
+      user_firstName: null,
+      user_lastName: null,
+      user_email: null,
+      user_password: null,
+      user_passwordConf: null
+    },
+    msg: {
+      user_firstName: null,
+      user_lastName: null,
+      user_email: null,
+      user_password: null,
+      user_passwordConf: null
+    }
+  };
+
+  res.render('register.ejs', result);
 });
 
 app.get('/login', (req, res) => {
@@ -35,8 +76,61 @@ app.get('/login', (req, res) => {
 });
 
 // Let User Make An Account
-app.post('/register', (req, res) => {
-  console.log(req.body);
+app.post('/register', (req, res, next) => {
+
+  let result = checker(req.body);
+
+  if (Object.keys(result.error).length > 0) {
+
+    return res.render('register.ejs', result);
+
+  } else {
+    connection.query(`SELECT id FROM users WHERE users.email = '${req.body.user_email}'`, (err, emails) => {
+
+      if (emails.length > 0) {
+        result.error.user_email = 'isInvalid';
+        result.data.user_email = null;
+        result.msg.user_email = 'Already in use';
+
+        return res.render('register.ejs', result)
+      } else {
+
+        let x = req.body.user_password;
+
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(x, salt, function(err, hash) {
+                if (err) {
+                  return next(err);
+                } else {
+                  x = hash;
+
+                  connection.query(`INSERT INTO users (email, password) VALUES(
+                    "${req.body.user_email}",
+                    "${x}"
+                  );
+                  INSERT INTO accounts (first_name, last_name, userID) VALUES(
+                    "${req.body.user_firstName}",
+                    "${req.body.user_lastName}",
+                    LAST_INSERT_ID()
+                  );`, (err, data) => {
+                    if (err) {
+                      next(err);
+                    } else {
+                      res.redirect('/');
+                    }
+                  });
+                }
+            });
+        });
+
+
+
+      }
+
+    })
+  }
+
+
 });
 
 // Let User Log In
