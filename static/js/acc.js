e41a0d24490eae23aa8b98fe7b6c71393388ed38
Jesse Dijkman <jesse.dijkman@hva.nl>
2018-06-21 16:11:58 +0200

    Added an account.html file for editing the user account

--- static/js/acc.js
+++ static/js/acc.js
@@ -5,6 +5,7 @@
 // ----------------------
 
 const accMenuItms = document.getElementsByClassName('acc-menu-itm');
+const likedRecipes = document.querySelectorAll('#acc-container section:last-of-type article .liked');
 
 accMenuItms[0].addEventListener('click', () => {
   document.body.classList.add('sec-1');
@@ -15,3 +16,11 @@ accMenuItms[1].addEventListener('click', () => {
   document.body.classList.add('sec-2');
   document.body.classList.remove('sec-1');
 });
+
+(function() {
+  for (let i = 0; i < likedRecipes.length; i++) {
+    likedRecipes[i].addEventListener('click', () => {
+      likedRecipes[i].classList.toggle('liked');
+    });
+  }
+})();
